4c5728310111a93e4495e5b4b582219be8db8206
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-07-09 23:54:14 +0200

    chore(front): add biome

--- front/app.vue
+++ front/app.vue
@@ -4,15 +4,17 @@ const isDark = computed({
   get: () => colorMode.value === 'dark',
   set: (newValue: boolean) => (colorMode.preference = newValue ? 'dark' : 'light'),
 })
+const language = navigator.language
 </script>
 
 <template>
   <UApp>
-       <UButton
-        :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
-        color="neutral"
-        variant="ghost"
-        @click="isDark = !isDark"
+    <Icon :name="`circle-flags:${language}`" size="20"/>
+    <UButton
+      :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+      color="neutral"
+      variant="ghost"
+      @click="isDark = !isDark"
     />
     <NuxtPage />
   </UApp>
